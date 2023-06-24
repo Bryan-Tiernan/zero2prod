2864a3742858fd9a0fd3b9581c7bd3019eac7037
Bryan Tiernan <bryantiernan@gmail.com>
2023-06-24 11:56:10 -0700

    fix dependencies

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,6 @@
 use std::net::TcpListener;
+use sqlx::{PgConnection, Connection};
+use zero2prod::configuration::get_configuration;
 
 #[tokio::test]
 async fn health_check_works() {
@@ -28,6 +30,11 @@ fn spawn_app() -> String {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data(){
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read config.");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
 
     let body = "name=bryan%20tiernan&email=bryantiernan%40gmail.com";
@@ -40,6 +47,14 @@ async fn subscribe_returns_a_200_for_valid_form_data(){
         .expect("Failed to execute request.");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch subscription.");
+
+    assert_eq!(saved.email, "bryantiernan@gmail.com");
+    assert_eq!(saved.name, "bryan tiernan");
 }
 
 #[tokio::test]
